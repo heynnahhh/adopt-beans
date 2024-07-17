24dadd56f0668519f6a0d9a93112ffa7df425a34
Hannah Liao <hannahpatricia.liao@gmail.com>
2024-07-18 03:26:15 +0800

    feat: Added logo images and class update for body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`inter.className`}>
+      <body className={`bg-ecru-white dark:bg-charcoal-grey ${inter.className}`}>
         <Header />
         {children}
         <Footer />
